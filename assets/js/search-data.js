0e328bfcf4b91ac13cff5f9124e06d767900690a
mikeshzhang <154236048+mikeshzhang@users.noreply.github.com>
2025-08-15 05:41:10 +0000

    Deploying to gh-pages from @ mikeshzhang/mikeshzhang.github.io@0dea16efe217c8151d96dee52be6262f5d4f640d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -435,8 +435,8 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
+      },{id: "books-culture-2-0",
+          title: 'Culture 2.0',
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
