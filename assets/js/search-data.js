bb750aab14a3923d4d4a391619c20f8099d341b8
mikeshzhang <154236048+mikeshzhang@users.noreply.github.com>
2025-08-17 15:18:17 +0000

    Deploying to gh-pages from @ mikeshzhang/mikeshzhang.github.io@7b08571f1ff1705eb81cbe101e018f2b2a6983b6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "Courses that I teach individually or as a teaching assistant will be shown here.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
