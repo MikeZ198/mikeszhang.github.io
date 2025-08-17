de0688336918c2ec8a1d3931c3c41447a13264e6
mikeshzhang <154236048+mikeshzhang@users.noreply.github.com>
2025-08-17 02:16:33 +0000

    Deploying to gh-pages from @ mikeshzhang/mikeshzhang.github.io@07ae120cd512d07d1a98370c8548eee8f58edc0a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,7 +46,7 @@ ninja.data = [{
           },
         },{id: "nav-people",
           title: "people",
-          description: "members of the lab or group",
+          description: "This page serves to pay tribute to, and publicise the nice collaborators I met (upon thier consent).",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
