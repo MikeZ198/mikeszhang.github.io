6ad34b4eede858f428266b24ffbbed1cc98b7d0f
mikeshzhang <154236048+mikeshzhang@users.noreply.github.com>
2025-08-17 01:12:37 +0000

    Deploying to gh-pages from @ mikeshzhang/mikeshzhang.github.io@8810ab6bb7ca2487c549c40b57dd34f14871e231 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Hopefully there will be something here in the future.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
